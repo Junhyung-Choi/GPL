dd3dfac7ef5c4abf755c0346453657a756f5aa67
Junhyung Choi <junhyung9571@naver.com>
2022-04-11 13:24:29 +0900

    First Commit

---
+++ Assets/MetroidvaniaController/Scripts/KillZone.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillZone : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+        else
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    private void Start() {
+        Debug.Log("ceil" + Mathf.Ceil(-2.3f));
+        Debug.Log("floor" + Mathf.Floor(-1.75f));
+    }
+}
