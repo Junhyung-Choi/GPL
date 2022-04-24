d63eac6668c289d47294527e83df197ddebd6bcd
LightYe4r <24adrenalin@naver.com>
2022-04-24 14:57:35 +0900

    test

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
-<<<<<<< HEAD
-    public string ItemName;
-=======
-    public string itemname;
->>>>>>> e3872d34e6d682b48c59ca5ca5964a02974d5b5a
+    public string Itemname;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         // userItem[itemName] = true;
