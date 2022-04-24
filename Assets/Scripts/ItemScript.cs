2c6146439c9b626cf42f8e0ffd5d3ceb8236d5f5
Junhyung Choi <junhyung9571@naver.com>
2022-04-24 15:12:52 +0900

    TEST - change namespace

--- Assets/Scripts/ItemScript.cs
+++ Assets/Scripts/ItemScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ItemScript : MonoBehaviour
 {
-    public string itemName;
+    public string itemname;
 
     private void OnTriggerEnter2D(Collider2D collision) {
         // userItem[itemName] = true;
