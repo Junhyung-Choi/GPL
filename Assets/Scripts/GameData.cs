d287947e5e6f49585b18315c6361546802efcda8
LightYe4r <24adrenalin@naver.com>
2022-04-24 15:36:35 +0900

    FIX Ditionary syntax error

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -8,11 +8,11 @@ public class GameData
 {
     public int herolevel = 0;
     public int offenselevel = 0;
-    public Dictionary<string, int> offensetree = (("damage", 0), ("accuracy", 0), ("rate", 0));
+    public Dictionary<string, int> offensetree = {{"damage", 0}, {"accuracy", 0}, {"rate", 0}};
     public int defenselevel = 0;
-    public Dictionary<string, int> defensetree = (("health", 0), ("armor", 0), ("healrate", 0));
+    public Dictionary<string, int> defensetree = {{"health", 0}, {"armor", 0}, {"healrate", 0}};
     public int utilitylevel = 0;
-    public Dictionary<string, int> utilitylevel = (("reward", 0), ("dodge", 0), ("critical", 0));
+    public Dictionary<string, int> utilitylevel = {{"reward", 0}, {"dodge", 0}, {"critical", 0}};
     public int gunshoplevel = 0;
     public int relicshoplevel = 0;
 }
