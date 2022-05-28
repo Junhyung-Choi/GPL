f3816cf8273889abfbdf093816c4ff9c067ad0fc
Junhyung-Choi <junhyung9571@naver.com>
2022-05-28 22:50:13 +0900

    Bar Script

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -5,22 +5,66 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider sliderHP;
-    public Slider sliderArmor;
+    Slider slider;
+    GameData characterData;
+    public string info;
+    int val;
+
+    private void Start() {
+        slider = transform.GetComponent<Slider>();
+        characterData = DataController.Instance._gameData;
+    }
     // Start is called before the first frame update
     void Update()
     {
-        PlayerHPbar();
+        DrawBar();
     }
 
-    public void PlayerHPbar() {
+    public void DrawBar() {
+        switch(info){
+            case "realHealth":
+            
+            case "Damage":
+                val = characterData.offensetree["damage"];
+                break;
+            case "Accuracy":
+                val = characterData.offensetree["accuracy"];
+                break;
+            case "Rate":
+                val = characterData.offensetree["rate"];
+                break;
+            case "Health":
+                val = characterData.offensetree["health"];
+
+                break;
+            case "Armor":
+                val = characterData.offensetree["armor"];
+
+                break;
+            case "Healrate":
+                val = characterData.offensetree["healrate"];
+
+                break;
+            case "Reward":
+                val = characterData.offensetree["reward"];
+
+                break;
+            case "Dodge":
+                val = characterData.offensetree["dodge"];
+
+                break;
+            case "Critical":
+                val = characterData.offensetree["critical"];
+                break;
+        }
         CharacterController2D Char = GameObject.Find("DrawCharacter-Changing").GetComponent<CharacterController2D>();
         float hp = Char.life;
-        sliderHP.value = hp / 10;
+        slider.value = hp / 10;
+
     }
 
-    public void PlayerArmourBar() {
-        int Armor = DataController.Instance._gameData.defensetree["armor"];
-        sliderArmor.value = Armor / 30;
-    }
+    // public void PlayerArmourBar() {
+    //     int Armor = DataController.Instance._gameData.defensetree["armor"];
+    //     sliderArmor.value = Armor / 30;
+    // }
 }
